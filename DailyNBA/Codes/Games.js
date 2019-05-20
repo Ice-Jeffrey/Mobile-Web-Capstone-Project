93ca1bda95c54e2ae10fdccb8d67c0b64ed4c2b6
Ice-Jeffrey <735429132@qq.com>
2019-05-20 16:53:35 +0800

    Update Games.js

--- DailyNBA/Codes/Games.js
+++ DailyNBA/Codes/Games.js
@@ -1,6 +1,6 @@
 import react, {Component} from 'react'
-import {View, Text, Image, StyleSheet, Dimensions} from 'react-native'
-import '../assets/address'
+import {View, Text, Image, StyleSheet, Dimensions, TouchableOpacity} from 'react-native'
+import address from '../assets/address'
 
 //get height and width of the window
 const Height = Dimensions.get('screen').height;
@@ -10,10 +10,36 @@ export class Games extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            address: address,
+            games: [
 
+            ]
         }
     }
 
+    //get the data from the internet
+    componentWillMount() {
+        fetch(this.state.address.gameGeneral)
+            .then(response => response.json() )
+            .then(data => {
+                this.setState({games: data});
+            })
+            .catch(error => alert(error))
+    }
+
+    render() 
+    {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity>
+                    <View style={styles.box}>
+
+                    </View>
+                </TouchableOpacity>
+            </View>
+            
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -25,5 +51,13 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         height: Height/6,
+    },
+    team: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    teamImage: {
+        
     }
   });
