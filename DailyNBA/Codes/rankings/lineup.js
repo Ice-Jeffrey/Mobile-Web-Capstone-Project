e359c2c1ec76a8dc38eb4fd9720ad413634608e9
Ice-Jeffrey <735429132@qq.com>
2019-06-06 20:50:21 +0800

    convert stateful components into stateless components

--- DailyNBA/Codes/rankings/lineup.js
+++ DailyNBA/Codes/rankings/lineup.js
@@ -7,79 +7,72 @@ const height = Dimensions.get('window').height;
 
 
 
-export default class lineup extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            name:this.props.screenProps.name,
-            info:[]
-        }
-    }
-    render(){
-        return(
-            <View style={{flexDirection:'row',margin:5}}>
-                <View style={{margin:5}}>
-                    <Text style={styles.title}>Name</Text>
-                    {playerinfo[this.state.name].basic.resultSets[0].rowSet.map((element,index)=>{
-                        return(
+export default lineup = ({props}) => {
 
-                            <View style={{flexDirection:'column',marginBottom:5}}>
-                                <Text style={styles.text}>{element[3]}</Text>                                            
-                            </View>                                                     
-                    )                            
-                    })}                    
-                </View>
-               
-                <ScrollView 
-                    horizontal={true}
-                    keyboardDismissMode={"on-drag"}
-                    showsHorizontalScrollIndicator={false}
-                >
-                    <View style={{margin:6}}>
-                        <View style={{flexDirection:'row'}}>
-                            <Text style={styles.title1}>Num.</Text>
-                            <Text style={styles.title1}>Pos.</Text>
-                            <Text style={styles.title1}>Age</Text>
-                            <Text style={styles.title1}>Hgt.</Text>
-                            <Text style={styles.title1}>Wgt.</Text>
-                            <Text style={{width:width/3,alignItems:'center',fontSize:20,fontWeight:'bold',color:'black'}}>Sch.</Text>
-                            <Text style={{width:width/2,alignItems:'center',fontSize:20,fontWeight:'bold',color:'black'}}>Bir.</Text>                            
-                        </View>
+    return(
+        <View style={{flexDirection:'row',margin:5}}>
+            <View style={{margin:5}}>
+                <Text style={styles.title}>Name</Text>
+                {playerinfo[props.name].basic.resultSets[0].rowSet.map((element,index)=>{
+                    return(
 
-                        {playerinfo[this.state.name].basic.resultSets[0].rowSet.map((element,index)=>{
-                            return(
-                                <View style={{flexDirection:'row'}}>
-                                    <View style={{flexDirection:'column',marginBottom:5}}>
-                                        <Text style={styles.text2}>{element[4]}</Text>                                     
-                                    </View>
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={styles.text2}>{element[5]}</Text>                                     
-                                    </View>    
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={styles.text2}>{element[9]}</Text>                                     
-                                    </View>    
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={styles.text2}>{element[6]}</Text>                                     
-                                    </View> 
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={styles.text2}>{element[7]}</Text>                                     
-                                    </View>       
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={{width:width/3,alignItems:'center',color:'black'}}>{element[11]}</Text>                                     
-                                    </View>             
-                                    <View style={{flexDirection:'column'}}>
-                                        <Text style={{width:width/2,alignItems:'center',color:'black'}}>{element[8]}</Text>                                     
-                                    </View>       
-                                </View>
-                        
-                        )                            
-                        })}                        
+                        <View style={{flexDirection:'column',marginBottom:5}}>
+                            <Text style={styles.text}>{element[3]}</Text>                                            
+                        </View>                                                     
+                )                            
+                })}                    
+            </View>
+            
+            <ScrollView 
+                horizontal={true}
+                keyboardDismissMode={"on-drag"}
+                showsHorizontalScrollIndicator={false}
+            >
+                <View style={{margin:6}}>
+                    <View style={{flexDirection:'row'}}>
+                        <Text style={styles.title1}>Num.</Text>
+                        <Text style={styles.title1}>Pos.</Text>
+                        <Text style={styles.title1}>Age</Text>
+                        <Text style={styles.title1}>Hgt.</Text>
+                        <Text style={styles.title1}>Wgt.</Text>
+                        <Text style={{width:width/3,alignItems:'center',fontSize:20,fontWeight:'bold',color:'black'}}>Sch.</Text>
+                        <Text style={{width:width/2,alignItems:'center',fontSize:20,fontWeight:'bold',color:'black'}}>Bir.</Text>                            
                     </View>
 
-                </ScrollView>               
-            </View>   
-        )
-    }
+                    {playerinfo[props.name].basic.resultSets[0].rowSet.map((element,index)=>{
+                        return(
+                            <View style={{flexDirection:'row'}}>
+                                <View style={{flexDirection:'column',marginBottom:5}}>
+                                    <Text style={styles.text2}>{element[4]}</Text>                                     
+                                </View>
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={styles.text2}>{element[5]}</Text>                                     
+                                </View>    
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={styles.text2}>{element[9]}</Text>                                     
+                                </View>    
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={styles.text2}>{element[6]}</Text>                                     
+                                </View> 
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={styles.text2}>{element[7]}</Text>                                     
+                                </View>       
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={{width:width/3,alignItems:'center',color:'black'}}>{element[11]}</Text>                                     
+                                </View>             
+                                <View style={{flexDirection:'column'}}>
+                                    <Text style={{width:width/2,alignItems:'center',color:'black'}}>{element[8]}</Text>                                     
+                                </View>       
+                            </View>
+                    
+                    )                            
+                    })}                        
+                </View>
+
+            </ScrollView>               
+        </View>   
+    )
+    
 }
 
 const styles = StyleSheet.create({
